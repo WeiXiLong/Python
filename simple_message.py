45eca98ea9814e867e18ef6837a3c0904f32ed48
WeiXiLong <438545362@qq.com>
2019-05-09 21:35:58 +0800

    Initial commit

---
+++ simple_message.py
@@ -0,0 +1,57 @@
+#message = 'Hello World!'
+#print(message)
+
+#message = 'Hello Python'
+#print(message)
+
+#name = 'Wei xi long'
+#print(name.title())
+
+#first_name = 'Wei'
+#last_name = 'xi long'
+#full_name = first_name + ' ' + last_name
+#print(full_name)
+#
+#message = 'Hello,' + '' + full_name.title() + '!'
+#print(message)
+
+#name = 'wei '
+#print(name)
+#name = name.rstrip()
+#print(name)
+
+#name = 'Eric'
+#print('Hello ' + name + ' would you like to learn some Python today?')
+
+#name = 'wei'
+#print(name.lower())
+#print(name.upper())
+#print(name.title())
+
+#name  = 'Albert Einstein'
+#message = '"A preson who never made a mistake never tried anything new."'
+#print(name  +  ' once said,' + message)
+
+#name = ' wei peng\t\n'
+#print(name)
+#name = name.rstrip()
+#print(name)
+#name = name.lstrip()
+#print(name)
+#name = name.strip()
+#print(name)
+
+
+#print(0.2 + 0.1)
+#print(3 * 0.1)
+
+#print(1+7)
+#print(16/2)
+#print(2*4)
+#print(9-1)
+
+
+#num = 1
+#print('My favorite num is ' + str(num))
+
+import this
